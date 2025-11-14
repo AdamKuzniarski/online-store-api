29d5e213cd1e903e96ebc9166d92f1d43a506f33
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-11-14 14:12:15 +0100

    add routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { Products2Module } from './products2/products.module';
-import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [ProductsModule, Products2Module],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true, 
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class ProductsService {
+  constructor(
+    @InjectRepository(Product) private readonly repo: Repository<Product>,
+  ) {}
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const entity = this.repo.create({ id: randomUUID(), ...createProductDto });
+    return this.repo.save(entity);
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.repo.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.repo.findOne({ where: { id } });
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const exists = await this.repo.findOne({ where: { id } });
+    if (!exists) {
+      throw new NotFoundException('Product not found');
+    }
+    return this.repo.save({ ...exists, updateProductDto, id });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    const res = await this.repo.delete(id);
+    if (res.affected === 0) throw new NotFoundException('Product not found');
   }
 }
