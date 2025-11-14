38be637838db6461297d335317db66c54488dac8
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-11-14 15:30:01 +0100

    add styling for static route

--- frontend/app/products/page.tsx
+++ frontend/app/products/page.tsx
@@ -9,13 +9,16 @@ export default function ProductsPage() {
     <main className="min-h-screen max-w-3xl mx-auto p-8">
       <h1 className="text-3xl font-extrabold mb-6">Produkte - statish!</h1>
 
-      <ul>
+      <ul className="grid gap-4 sm:grid-cols-2">
         {products.map((product) => {
           product;
           return (
-            <li key={product.id} className="grid gap-4 sm:grid-cols-2">
-              <h2>{product.name}</h2>
-              <span>{product.price}€</span>
+            <li
+              className="rounded-xl border border-slate-700 p-4 flex flex-col gap-2"
+              key={product.id}
+            >
+              <h2 className="text-xl font-semibold">{product.name}</h2>
+              <span className="text-sm text-slate-400">{product.price}€</span>
             </li>
           );
         })}
